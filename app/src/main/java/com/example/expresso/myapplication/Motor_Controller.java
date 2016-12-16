9b5a220708ebab1c6e7285c83ba88dea7ceb46a6
donborito <donborito@bitbucket.org>
2016-12-16 13:42:41 +0100

    Mit motor ausschalten ohne das der motor geräusche macht.

--- app/src/main/java/com/example/expresso/myapplication/Motor_Controller.java
+++ app/src/main/java/com/example/expresso/myapplication/Motor_Controller.java
@@ -5,9 +5,13 @@ package com.example.expresso.myapplication;
  */
 
 public class Motor_Controller {
-    public enum Motor_Side {left, right};
+    public enum Motor_Side {left, right}
 
-    public enum Driving_Direction {forwards, backwards, none};
+    ;
+
+    public enum Driving_Direction {forwards, backwards, none}
+
+    ;
 
     public void drive(Motor_Side motor_side, Driving_Direction direction) {
 
@@ -15,6 +19,8 @@ public class Motor_Controller {
         // motor id
         byte motor = 0x00;
         byte motorSpeed = 0x00;
+        byte motorRunState = 0x20;
+        byte motorRegulationMode = 0x01;
         // select motor id
         switch (motor_side) {
             case left:
@@ -28,19 +34,21 @@ public class Motor_Controller {
         switch (direction) {
             case forwards:
                 motorSpeed = 0x32;
-
                 break;
             case backwards:
-
                 motorSpeed = -0x32;
-
+                break;
+            case none:
+                motorRegulationMode = 0x00;
+                motorSpeed = 0x00;
+                motorRunState = 0x00;
                 break;
             default:
                 motorSpeed = 0x00;
                 break;
         }
         // create command byte
-        command = new byte[]{0x0C,0x00,0x00,0x04,motor,motorSpeed,0x05,0x01,motorSpeed,0x20,0x00,0x00,0x00,0x00};
+        command = new byte[]{0x0C, 0x00, 0x00, 0x04, motor, motorSpeed, 0x05, motorRegulationMode, motorSpeed, motorRunState, 0x00, 0x00, 0x00, 0x00};
         // send command to robot
         Global.connector.send(command);
 
